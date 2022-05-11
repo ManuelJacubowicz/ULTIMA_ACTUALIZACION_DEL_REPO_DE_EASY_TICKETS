2a70e940359d9838cc8b58b4cee6a9fb3ca67ffe
abrilmassoia <abril_anto@hotmail.com>
2022-05-10 23:15:28 -0300

    hola

---
+++ routes/rutasProductDetail.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+const productController = require('../controllers/productDetailcontroller')
+
+router.get('/', productController.index);
+
+router.get('/create', productController.create);
+router.post('/create', productController.storeNew);
+
+router.get('/:id/edit', productController.edit);
+router.put('/:id/edit', productController.storeEdit);
+
+router.get('/:id', productController.detail);
+
+
+module.exports = router;
